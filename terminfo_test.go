20643801e8efdf681ae27fd30f9c0e47649784c8
Brett Vickers <brett@beevik.com>
2018-05-05 00:41:03 -0700

    add standard capability constants.

--- terminfo_test.go
+++ terminfo_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestRead(t *testing.T) {
-	file, err := os.Open("/usr/share/terminfo/x/xterm1")
+	file, err := os.Open("./_data/xterm-256color")
 	if err != nil {
 		t.Errorf("%v\n", err)
 	}
@@ -17,5 +17,23 @@ func TestRead(t *testing.T) {
 		t.Errorf("%v\n", err)
 	}
 
-	_ = ti
+	if len(ti.Names) != 2 {
+		t.Errorf("Expected 2 names, got %d\n", len(ti.Names))
+	}
+
+	if ti.Names[0] != "xterm-256color" || ti.Names[1] != "xterm with 256 colors" {
+		t.Errorf("Name mismatch\n")
+	}
+}
+
+func TestConstants(t *testing.T) {
+	if BoolLPIChangesRes != 36 {
+		t.Error("Boolean constant mismatch")
+	}
+	if NumBitImageType != 32 {
+		t.Error("Number constant mismatch")
+	}
+	if StrSetPglenInch != 393 {
+		t.Error("String constant mismatch")
+	}
 }
